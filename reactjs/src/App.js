985563a23ee7759ca4fb3d189b0af2763425585a
unknown <PericlesDosSantosRamos@hotmail.com>
2022-05-01 20:42:54 -0300

    Primeira linha de código com React.js

---
+++ reactjs/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function app(){
+  return (
+    <div>
+      <h1>Olá mundo!</h1>
+      <p>Meu primeiro projeto commitado em React :)</p>
+    </div>
+  );
+
+}
+
+export default app;
---
+++ reactjs/src/topo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function topo(){
+    return (
+          <div>
+              Topo do Site
+          </div>
+
+    );
+
+}
+
+export default topo;
