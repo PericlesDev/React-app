fc79ae8cfcf163133e64c332af380372c1032370
unknown <PericlesDosSantosRamos@hotmail.com>
2022-05-04 09:17:31 -0300

    Function Button

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function app(){
-  return (
-    <div>
-      <h1>Olá mundo!</h1>
-      <p>Meu primeiro projeto commitado em React :)</p>
-    </div>
-  );
 
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+
+
+    this.state = {
+      showImage: false,
+    };
+  }
+
+  toggle = () => {
+    this.setState ({
+      showImage: !this.state.showImage,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+    <header className="App-header">
+      <button type="button" onClick={this.toggle}>
+      {this.state.showImage ? 'Esconder' : 'Mostrar'}
+      </button>
+      {this.state.showImage && (
+      <img src={logo} className="App-logo" alt="logo" />
+      )}
+    <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
